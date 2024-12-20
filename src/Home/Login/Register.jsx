1b010b344be3dbb62d875749bf334db096a58119
shohelrana2002 <rana2021tang@gmail.com>
2024-12-20 18:40:13 +0600

    Add to Fierbase , env AuthProvider

--- src/Home/Login/Register.jsx
+++ src/Home/Login/Register.jsx
@@ -13,10 +13,11 @@ const Register = () => {
     console.log(data.name);
     const fistName = data.name;
     const lastName = data.lastName;
+    const status = role === "user" ? "approved" : "pending";
   };
   return (
     <div>
-      <div className="hero bg-base-200 min-h-screen">
+      <div className="hero py-8 bg-base-200 min-h-screen">
         <div className="flex justify-between lg:gap-28 items-center flex-col lg:flex-row">
           <div className="w-full">
             <picture>
@@ -49,11 +50,15 @@ const Register = () => {
                   type="text"
                   placeholder="Last Name"
                   className="input input-bordered"
-                  {...register("lastName", { required: true })}
+                  {...register("lastName", {
+                    required: true,
+                    pattern: /^[A-Za-z]+$/i,
+                  })}
                 />
                 {errors.lastName && (
                   <span className="text-red-700">Last Name is Required </span>
                 )}
+                {errors.pattern && <span>hi</span>}
               </div>
               {/* Email */}
               <div className="form-control">
@@ -69,6 +74,58 @@ const Register = () => {
                 {errors.email && (
                   <span className="text-red-700">Email is Required </span>
                 )}
+              </div>
+              {/* Password */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="input input-bordered"
+                  {...register("password", { required: true, min: 6 })}
+                />
+                {errors.password && (
+                  <span className="text-red-700">password is Required </span>
+                )}
+              </div>
+              {/* confirm Password */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Confirm Password </span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  className="input input-bordered"
+                  {...register("ConfirmPassword", {
+                    required: "Confirm Password is Required",
+                    min: 6,
+                    validate: (value) =>
+                      watch("password") === value ||
+                      "Your Password Don't Match",
+                  })}
+                />
+                {errors.ConfirmPassword && (
+                  <span className="text-red-700">
+                    {errors.ConfirmPassword.message}
+                  </span>
+                )}
+              </div>
+              {/* Role  */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Role</span>
+                </label>
+                <select
+                  className="select select-bordered w-full max-w-full                  "
+                  {...register("role", { required: true })}
+                >
+                  <option value="buyer">User</option>
+                  <option value="seller">Admin</option>
+                </select>
+                {errors.role && <p>Select On</p>}
               </div>
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Login</button>
