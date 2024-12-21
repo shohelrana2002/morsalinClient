3a9e053c928468232d69baf8b03425e3c9a9b13c
shohelrana2002 <rana2021tang@gmail.com>
2024-12-21 20:50:57 +0600

    Gmail Password Reset

--- src/Home/Login/ForgetPassword.jsx
+++ src/Home/Login/ForgetPassword.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import useGetAuth from "../../Hooks/useGetAuth";
-
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Swal from "sweetalert2";
 const ForgetPassword = () => {
-  const [message, setMessage] = useState("");
-  const [error, setError] = useState("");
-
   const { forgetPassword } = useGetAuth();
   const {
     register,
@@ -17,18 +16,29 @@ const ForgetPassword = () => {
     console.log(data.email);
     forgetPassword(data?.email)
       .then(() => {
-        setMessage("Password reset email sent! Check your inbox.");
-        setError("");
+        Swal.fire({
+          position: "top",
+          width: "400px",
+          icon: "success",
+          title: "Check your Gmail",
+          showConfirmButton: false,
+          timer: 1500,
+        });
       })
       .catch((err) => {
-        setError(err.message || "Failed to send password reset email.");
-        setMessage("");
+        Swal.fire({
+          position: "top-end",
+          icon: "error",
+          title: "Invalid Yor Gmail or Account",
+          showConfirmButton: false,
+          timer: 1500,
+        });
       });
   };
 
   return (
     <div className="hero bg-base-200 min-h-screen  ">
-      <div className="flex items-center justify-between flex-col md:flex-row-reverse">
+      <div className="flex items-center lg:gap-x-32 justify-between flex-col md:flex-row-reverse">
         <div>
           <div className="w-full">
             <picture>
@@ -38,10 +48,13 @@ const ForgetPassword = () => {
         </div>
 
         <div>
+          <h3 className="text-2xl text-center font-semibold text-gray-700">
+            Forget Your Password
+          </h3>
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Email </span>
+                <span className="label-text">Enter Your Email </span>
               </label>
               <input
                 type="email"
@@ -60,8 +73,6 @@ const ForgetPassword = () => {
               Send Password Reset Email
             </button>
           </form>
-          {message && <p style={{ color: "green" }}>{message}</p>}
-          {error && <p style={{ color: "red" }}>{error}</p>}
         </div>
       </div>
     </div>
