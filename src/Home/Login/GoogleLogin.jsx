cb7680c3c62b852e8fc56b584dd248d80794ffcf
shohelrana2002 <rana2021tang@gmail.com>
2024-12-22 10:49:51 +0600

    Google login Success and error react totsy  alert messsage set

--- src/Home/Login/GoogleLogin.jsx
+++ src/Home/Login/GoogleLogin.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { IoLogoGoogle } from "react-icons/io5";
 import useGetAuth from "../../Hooks/useGetAuth";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const GoogleLogin = () => {
   const { googleLogin } = useGetAuth();
   const handleGoogleLogin = () => {
@@ -8,19 +10,36 @@ const GoogleLogin = () => {
       .then((res) => {
         if (!res?.user?.emailVerified) {
           userLogout();
-          return alert(
-            "Your email is not verified. Please verify your email to continue."
+          toast.error(
+            "Your email is not verified. Please verify your email to continue.",
+            {
+              position: "top-right",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+            }
           );
+          return;
         }
+        toast.success("Login successful!", {
+          position: "top-right",
+          autoClose: 3000,
+        });
         console.log(res.user);
       })
       .catch((err) => {
-        console.log(err.message);
+        toast.error(`Error: ${err.message}`, {
+          position: "top-right",
+          autoClose: 5000,
+        });
       });
   };
   return (
     <div className="">
       <p className="cursor-pointer" onClick={handleGoogleLogin}>
+        <ToastContainer />
         <IoLogoGoogle className="text-center w-full text-green-700 text-2xl"></IoLogoGoogle>
       </p>
     </div>
